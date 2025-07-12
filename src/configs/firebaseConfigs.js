ed77c065448397db5c0b396736e028201e7666fc
devz_ikko <jerichojamesgonzaga13@gmail.com>
2025-07-12 23:46:03 +0800

    edit the env

--- src/configs/firebaseConfigs.js
+++ src/configs/firebaseConfigs.js
@@ -5,16 +5,15 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCYztJsDBVoD4dxr381ScszSBwwDFDAG7c",
-  authDomain: "dcphlogs.firebaseapp.com",
-  projectId: "dcphlogs",
-  storageBucket: "dcphlogs.firebasestorage.app",
-  messagingSenderId: "79134966025",
-  appId: "1:79134966025:web:e848fa2bf6e44ca93593d8",
-  measurementId: "G-EXKF0XXR49"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
